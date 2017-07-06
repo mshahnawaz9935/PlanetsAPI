e2aeb4b4716d796ed3cfb3aa3a77332e0b920a5f
Shahnawaz Alam <mshahnawaz9935@gmail.com>
2017-07-06 01:20:58 +0100

    update

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlanetsAPI.Global" Language="C#" %>
---
+++ Models/Planets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanetsAPI.Models
+{
+    public class Planets
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string distance { get; set; }
+
+    }
+}
